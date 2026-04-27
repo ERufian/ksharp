e8741e12ed56be6d45991cca9e0e2b8c0587e69c
ERufian <github@rufian.zilbermann.com>
2026-04-27 03:21:17 -0400

    Add comments not to run comparison tool for testing purposes

--- run_comparison.bat
+++ run_comparison.bat
@@ -1,4 +1,6 @@
 @echo off
+rem DO NOT RUN COMPARISONS FOR TESTING PURPOSES. IT IS VERY INEFFICIENT
+rem USE THE RUN TEST SKILL FOR RUNNING TESTS
 echo Running K3Sharp vs k.exe Comparison...
 cd /d "t:\_src\github.com\ERufian\ksharp\K3CSharp.Comparison"
 dotnet run
